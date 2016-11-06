a01a8eb09148c35d25b80acdd663198b650085fa
Bùi Quốc Hưng <Bi Quc Hng>
2016-11-06 20:38:42 +0700

    Fixed #11 Cau lenh khong can thiet

--- vn/vanlanguni/ponggame/PongPanel.java
+++ vn/vanlanguni/ponggame/PongPanel.java
@@ -270,7 +270,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			// Draw Restart message
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 18));
 			g.drawString("Press 'SPACE' to Restart the game.", 175, 400);
-			// TODO Draw a restart message
+		
 		}
 	}
 
